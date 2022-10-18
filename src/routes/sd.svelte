881450b66d907e9d7538918cbad43b3c2061c3f9
Caleb Bodtorf <cbodtorf@gmail.com>
2022-10-17 22:26:54 -0400

    feat: style updates to sd.

--- src/routes/sd.svelte
+++ src/routes/sd.svelte
@@ -5,7 +5,7 @@
     <legend>Mexican Food</legend>
     <article>
       <div>
-        <ol>
+        <ul>
           <li><a target="_blank" class="mp-js" href="https://www.elindiosandiego.net/">El Indio</a></li>
           <ul><li>a murphy family staple since the 70s</li></ul>
           <li><a target="_blank" class="mp-js" href="https://www.eltianguisrt.com/">El Tianguis</a></li>
@@ -14,7 +14,7 @@
           <ul><li>everything you've you always wanted taco bell to be</li></ul>
           <li><a target="_blank" class="mp-js" href="https://raulsshack.com/">Raul's Shack</a></li>
           <ul><li>big burritos by the beach</li></ul>
-        </ol>
+        </ul>
       </div>
     </article>
   </fieldset>
@@ -24,7 +24,7 @@
     <legend>Restaurants</legend>
     <article>
       <div>
-        <ol>
+        <ul>
           <li><a target="_blank" class="mp-js" href="https://morningglorybreakfast.com/">Morning Glory</a></li>
           <ul><li>over-the-top brunch spot. great food, great drinks, great vibes</li></ul>
           <li><a target="_blank" class="mp-js" href="https://www.jeune-jolie.com/">Jeune et Jolie</a></li>
@@ -33,7 +33,7 @@
           <ul><li>great ramen and drinks</li></ul>
           <li><a target="_blank" class="mp-js" href="https://www.tributepizza.com/">Tribute Pizza</a></li>
           <ul><li>its pizza, its good, what else do you need</li></ul>
-        </ol>
+        </ul>
       </div>
     </article>
   </fieldset>
@@ -43,14 +43,14 @@
     <legend>Coffee</legend>
     <article>
       <div>
-        <ol>
+        <ul>
           <li><a target="_blank" class="mp-js" href="https://communalcoffee.com/">Communal Coffee</a></li>
           <ul><li>community focused coffee + flowers + great seasonal breakfast foods</li></ul>
           <li><a target="_blank" class="mp-js" href="https://www.birdrockcoffee.com/">Bird Rock Coffee</a></li>
           <ul><li>espresso for the connoisseur, multiple locations around san diego</li></ul>
           <li><a target="_blank" class="mp-js" href="https://betterbuzzcoffee.com/">Better Buzz</a></li>
           <ul><li>get the coconut buzz... you won't regret it</li></ul>
-        </ol>
+        </ul>
       </div>
     </article>
   </fieldset>
@@ -60,7 +60,7 @@
     <legend>Outdoors</legend>
     <article>
       <div>
-        <ol>
+        <ul>
           <li><a target="_blank" class="mp-js" href="/assets/RBI__Hiking_Map__Final.pdf">RB Inn Hiking Trails</a></li>
           <ul><li>for easy walks around the venue</li></ul>
           <li><a target="_blank" class="mp-js" href="https://zoo.sandiegozoo.org/">San Diego Zoo</a></li>
@@ -73,7 +73,7 @@
           <ul><li>our favorite hiking spot during the pandemic</li></ul>
           <li><a target="_blank" class="mp-js" href="https://torreypine.org/home-2-2/reserveinfo/trails/">Torrey Pines</a></li>
           <ul><li>nature reserve with unspoiled pacific coast wonders</li></ul>
-        </ol>
+        </ul>
       </div>
     </article>
   </fieldset>
@@ -82,6 +82,7 @@
 <style>
   a {
     color: var(--color-light-green);
+    font-weight: bold;
   }
   legend, h1 {
     color: var(--color-gray-1000);
@@ -93,15 +94,15 @@
     max-width: 60rem;
     margin: 0 auto;
   }
-  article p {
-    text-align: center;
+  article li {
     font-family: var(--font-form);
     font-family: var(--mono);
-    margin-bottom: 30px;
+    margin-bottom: 10px;
   }
 
   fieldset {
     margin-top: 25px;
+    padding-left: 1.75em;
   }
 
   article {
